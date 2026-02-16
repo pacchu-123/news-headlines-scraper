741cf693243b5e9debf846794ffb0b5c1e2066c9
pacchu-123 <prasannapatil9353@gmail.com>
2026-02-16 20:14:33 +0530

    Update news_scraper.py

--- news_scraper.py
+++ news_scraper.py
@@ -9,7 +9,8 @@ soup = BeautifulSoup(response.text, "html.parser")
 headlines = soup.find_all("h2")
 
 with open("head_lines.txt", "w", encoding="utf-8") as f:
-    for i, h in enumerate(headlines[:10], 1):
+    for i, h in enumerate(headlines[:5], 1):
         f.write(f"{i}. {h.text.strip()}\n")
 
 print("Headlines saved successfully!")
+
